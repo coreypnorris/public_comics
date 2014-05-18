8353c09c635325e12d85b589cba11a993441ad18
Corey Norris <coreypnorris@gmail.com>
2014-05-18 10:26:37 -0700

    delete unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 PublicComics::Application.routes.draw do
   root to: 'welcome#index'
 
-  resources :titles do
-    resources :issues do
-      resources :pages
+  resources :titles, only: [:index] do
+    resources :issues, only: [:index] do
+      resources :pages, only: [:show]
     end
   end
-  resources :issues
-  resources :pages
-  resources :genres
 
-  match 'title/issue/page', :to => 'pages#show', :via => :post
+  resources :genres, only: [:show]
+
 end
