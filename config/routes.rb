2528e7833cc68ec8aecfca00ea6dff8fa5a1241a
Corey Norris <coreypnorris@gmail.com>
2014-05-21 08:41:53 -0700

    simplify routes and refactor links accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 PublicComics::Application.routes.draw do
-
-
   resources :titles, only: [:index] do
-    resources :issues, only: [:index] do
-      resources :pages, only: [:show]
-    end
+    resources :issues, :name_prefix => "title_"
   end
 
+  resources :issues, only: [:index] do
+    resources :pages, :name_prefix => "issue_"
+  end
+
+  resources :pages, only: [:show]
+
   resources :genres, only: [:show]
+
   root to: 'welcome#index'
 end
+
+
+
