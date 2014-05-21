3d2fb144eaa8fb8ece5ab3f31a68e1f89f908076
Corey Norris <coreypnorris@gmail.com>
2014-05-21 08:44:24 -0700

    simplify routes and refactor links accordingly

--- spec/features/pages_pages_spec.rb
+++ spec/features/pages_pages_spec.rb
@@ -4,7 +4,7 @@ feature "Viewing a comic book page" do
 
   scenario "User sees cover of comic" do
     comic = FactoryGirl.create(:page).issue
-    visit title_issue_page_path(comic.title, comic, comic.pages.first)
+    visit issue_page_path(comic, comic.pages.first)
 
     page.find('img')['src'].should have_content comic.pages.first.image
   end
