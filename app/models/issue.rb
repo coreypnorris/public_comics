c226c9cdd69cfb8d5f4b38081bb3742719a58560
Corey Norris <coreypnorris@gmail.com>
2014-07-10 10:06:52 -0700

    make issues commentable

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,7 +1,10 @@
 class Issue < ActiveRecord::Base
+  acts_as_commentable
+
   validates :number, :presence => true
   validates :cover, :presence => true
 
   has_many :pages
+  has_many :comments
   belongs_to :title
 end
