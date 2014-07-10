d77c19760f51ba56268077aa87ea16e02dcc5691
Corey Norris <coreypnorris@gmail.com>
2014-07-10 09:44:54 -0700

    create unit tests for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :username, :presence => true
+  has_many :comments
 end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Comment do
+  it { should validate_presence_of :body }
+
+  it { should belong_to :user }
+end
