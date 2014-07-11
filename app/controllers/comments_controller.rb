fb1f1337f852bd35aabc209117816594ccab1c14
Corey Norris <coreypnorris@gmail.com>
2014-07-10 19:06:30 -0700

    refactor comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,14 @@ class CommentsController < ApplicationController
   def create
     @parent = Issue.find(params[:issue_id]) if params[:issue_id]
     @parent = Comment.find(params[:comment][:parent_id]) if params[:comment][:parent_id]
+    @page = Page.find(params[:page_id]) if params[:page_id]
     @issue = @parent.class == Issue ? @parent : @parent.commentable
     @comment = Comment.build_from( @issue, current_user.id, comment_params[:body] )
     if @comment.save
       current_user.comments << @comment
       flash[:notice] = "Your comment has been submitted."
       if params[:issue_id]
-        redirect_to issue_path(@parent)
+        redirect_to :back
       elsif params[:comment][:parent_id]
         @comment.move_to_child_of(@parent)
         redirect_to issue_path(@parent.commentable)
