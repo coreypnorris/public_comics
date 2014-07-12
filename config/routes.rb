6bfb121a4d01f7392f02f03ba80250cd3658783c
Corey Norris <coreypnorris@gmail.com>
2014-07-12 10:33:40 -0700

    create routes for reply comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PublicComics::Application.routes.draw do
   devise_for :user, :path => '', :path_names => { :sign_in => "sign_in", :sign_out => "sign_out", :sign_up => "sign_up" }
+
   resources :titles, only: [:index] do
     resources :issues, :name_prefix => "title_"
   end
@@ -7,6 +8,10 @@ PublicComics::Application.routes.draw do
   resources :issues, only: [:index] do
     resources :pages, :name_prefix => "issue_"
     resources :comments, only: [:create]
+  end
+
+  resources :comments, only: [:new, :create, :show] do
+    resources :comments, only: [:new, :create]
   end
 
   resources :genres, only: [:show]
