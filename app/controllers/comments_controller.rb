b1828bb222917be6e393e8cf40ece655920ffbef
Corey Norris <coreypnorris@gmail.com>
2014-07-15 14:08:50 -0700

    refactor new and create methods for nested comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,27 +2,22 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!, only: [:create]
 
   def new
-    @comment = Comment.new(:parent_id => params[:parent_id])
-    @page = Page.find(params[:page_id])
+    @comment = Comment.new(:parent_id => params[:comment_id])
+    @parent_comment = Comment.find(params[:comment_id])
   end
 
   def create
-    @parent = Issue.find(params[:issue_id]) if params[:issue_id]
-    @parent = Comment.find(params[:comment][:parent_id]) if params[:comment][:parent_id]
-    @page = Page.find(params[:page_id]) if params[:page_id]
-    @issue = @parent.class == Issue ? @parent : @parent.commentable
+    @issue = params[:issue_id] ? Issue.find(params[:issue_id]) : Comment.find(params[:comment_id]).commentable
+    @parent_comment = Comment.find(params[:comment_id]) if params[:comment_id]
     @comment = Comment.build_from( @issue, current_user.id, comment_params[:body] )
     if @comment.save
       current_user.comments << @comment
       flash[:notice] = "Your comment has been submitted."
       if params[:issue_id]
-        respond_to do |format|
-          format.html { redirect_to :back }
-          format.js
-        end
-      elsif params[:comment][:parent_id]
-        @comment.move_to_child_of(@parent)
-        redirect_to issue_path(@parent.commentable)
+        redirect_to :back
+      elsif params[:comment_id]
+        @comment.move_to_child_of(@parent_comment)
+        redirect_to :back
       end
     else
       flash[:error] = "Something went wrong. Please try to save your comment again."
