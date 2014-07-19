3fc4e34ce11a2b89784bf1a60dcc3a9c14d83da2
Corey Norris <coreypnorris@gmail.com>
2014-07-19 11:04:58 -0700

    make vote buttons undo a vote if comment has been voted on

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,11 @@ class VotesController < ApplicationController
   def new
     @comment = Comment.find(params[:comment_id])
     @page = Page.find(params[:page_id])
-    if params[:kind] == "upvote"
+    if current_user.voted_for? @comment
+      @comment.unliked_by current_user
+      @comment.undisliked_by current_user
+      redirect_to issue_page_path(@page.issue, @page)
+    elsif params[:kind] == "upvote"
       @comment.liked_by current_user
       redirect_to issue_page_path(@page.issue, @page)
     elsif params[:kind] == "downvote"
