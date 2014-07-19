8b03ed6451fc2639eba511c78184d86661d9f59b
Corey Norris <coreypnorris@gmail.com>
2014-07-19 10:50:04 -0700

    create sliced_time_ago method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,15 @@
 class VotesController < ApplicationController
 
   def new
-    ;lkj
+    @comment = Comment.find(params[:comment_id])
+    @page = Page.find(params[:page_id])
+    if params[:kind] == "upvote"
+      @comment.liked_by current_user
+      redirect_to issue_page_path(@page.issue, @page)
+    elsif params[:kind] == "downvote"
+      @comment.downvote_from current_user
+      redirect_to issue_page_path(@page.issue, @page)
+    end
   end
 
 end
