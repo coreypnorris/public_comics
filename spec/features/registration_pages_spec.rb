7f41a923247411fdb7723a381802d0e208d10a4b
Corey Norris <coreypnorris@gmail.com>
2014-07-21 11:51:39 -0700

    create tests for registration pages when a user is signed in

--- spec/features/registration_pages_spec.rb
+++ spec/features/registration_pages_spec.rb
@@ -29,3 +29,41 @@ feature 'Signing Up' do
     page.should have_content 'match'
   end
 end
+
+feature 'Signed in' do
+  scenario "can sign out" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    visit root_path
+    click_link "Sign Out"
+    page.should have_content "successfully"
+  end
+
+  scenario "navbar should have link to profile page" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    visit root_path
+    page.should have_content "Signed in as #{user.username}"
+  end
+
+  scenario "navbar should not have a 'sign up' link" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    visit root_path
+    page.should_not have_content "Sign Up"
+  end
+
+  scenario 'site should block access to signing up' do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    visit new_user_registration_path
+    page.should have_content "You are already signed in."
+  end
+
+  scenario 'site should block access to signing in' do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    visit new_user_session_path
+    page.should have_content "You are already signed in."
+  end
+end
