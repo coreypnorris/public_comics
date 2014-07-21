6269b4e06ebc6115a4436130d425aaf416205869
Corey Norris <coreypnorris@gmail.com>
2014-07-21 10:40:15 -0700

    refactor test to make use of user factory

--- spec/features/registration_pages_spec.rb
+++ spec/features/registration_pages_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 feature 'Signing Up' do
   scenario 'with valid inputs' do
+    user = FactoryGirl.build(:user)
     visit new_user_registration_path
-    fill_in 'Username', :with => "snapper"
-    fill_in 'Email', :with => "snapper_carr@jla.org"
-    fill_in 'Password', :with => "foobarbaz"
-    fill_in 'Password Confirmation', :with => "foobarbaz"
+    fill_in 'Username', :with => user.username
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => user.password
+    fill_in 'Password Confirmation', :with => user.password_confirmation
     click_button "Sign up"
     page.should have_content 'successfully'
   end
