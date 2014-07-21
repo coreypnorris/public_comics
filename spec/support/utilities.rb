f9600d34f6be60b55fbbda00ddac12505db5ea97
Corey Norris <coreypnorris@gmail.com>
2014-07-21 10:31:43 -0700

    create sign_in utility method

---
+++ spec/support/utilities.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+def sign_in(user)
+  visit new_user_session_path
+  fill_in "Username", :with => user.username
+  fill_in "Password", :with => user.password
+  click_button "Sign in"
+end
