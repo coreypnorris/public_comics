3af56410edfe47646f548111d0f4d2b959980e91
Corey Norris <coreypnorris@gmail.com>
2014-07-21 11:28:33 -0700

    refactor homepage test

--- spec/features/welcome_pages_spec.rb
+++ spec/features/welcome_pages_spec.rb
@@ -2,14 +2,18 @@ require 'spec_helper'
 
 feature "Viewing home page" do
 
-  scenario "User sees 12 latest issues" do
-    old_issue = FactoryGirl.create(:page).issue
+  scenario "User sees 12 latest issues and their covers" do
+    action_comics = FactoryGirl.create(:title, :name => "Action Comics")
+    action_comics_1 = action_comics.issues.create(:number => 1, :cover =>"action_comics_1_cover")
+    action_comics_1.pages.create(:number => 1, :image => "page_1_image_url")
     11.times { FactoryGirl.create(:page).issue }
-    new_issue = FactoryGirl.create(:page).issue
-    visit root_path
+    detective_comics = FactoryGirl.create(:title, :name => "Detective Comics")
+    detective_comics_27 = detective_comics.issues.create(:number => 27, :cover =>"detective_comics_27_cover")
+    detective_comics_27.pages.create(:number => 1, :image => "page_1_image_url")
+    visit root_url
 
-    page.should have_content new_issue.title.name
-    page.should_not have_content old_issue.title.name
+    page.should have_content detective_comics.name, detective_comics_27.cover
+    page.should_not have_content action_comics.name, action_comics_1.cover
   end
 
   scenario "User can use pagination links to see issues added earlier" do
@@ -38,6 +42,7 @@ feature "Viewing home page" do
 
     select("Horror", :from => "genre-selector")
     click_button "Go to Genre"
+
     page.should have_content tomb_of_dracula_1.title.name
   end
 
