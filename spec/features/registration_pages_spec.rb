f16e7d6589fba4e4e56708618c4eb485acfd781a
Corey Norris <coreypnorris@gmail.com>
2014-07-21 10:34:08 -0700

    create integration test for signing up as a new user

---
+++ spec/features/registration_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'Signing Up' do
+  scenario 'with valid inputs' do
+    visit new_user_registration_path
+    fill_in 'Username', :with => "snapper"
+    fill_in 'Email', :with => "snapper_carr@jla.org"
+    fill_in 'Password', :with => "foobarbaz"
+    fill_in 'Password Confirmation', :with => "foobarbaz"
+    click_button "Sign up"
+    page.should have_content 'successfully'
+  end
+end
