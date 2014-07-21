8f8b30b2f6d8c3ec5072cc404749fd8b6cb367d0
Corey Norris <coreypnorris@gmail.com>
2014-07-21 15:02:03 -0700

    write integration tests for editing account information

--- spec/features/registration_pages_spec.rb
+++ spec/features/registration_pages_spec.rb
@@ -30,11 +30,20 @@ feature 'Signing Up' do
   end
 end
 
-feature 'Signed in' do
+feature 'Signing in' do
   scenario "can sign in" do
     user = FactoryGirl.create(:user)
     sign_in(user)
     page.should have_content "successfully"
+  end
+
+  scenario "using incorrect information" do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in "Username", :with => "Wrong Username"
+    fill_in "Password", :with => user.password
+    click_button "Sign in"
+    page.should have_content 'Invalid'
   end
 
   scenario "can sign out" do
@@ -71,5 +80,42 @@ feature 'Signed in' do
     sign_in(user)
     visit new_user_session_path
     page.should have_content "You are already signed in."
+  end
+
+  feature "Editing account" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in(user) }
+
+    scenario "logged in user is able to access edit account form" do
+      visit root_path
+      click_link "Signed in as #{user.username}"
+      click_link "Edit or cancel your account"
+      page.should have_content "Edit Account"
+    end
+
+    scenario "logged in user is able to change their username" do
+      visit edit_user_registration_path
+      fill_in "Username", with: "NewUsername"
+      fill_in "Current Password", with: user.password
+      click_button "Update"
+      page.should have_content "successfully"
+    end
+
+    scenario "logged in user is able to change their email" do
+      visit edit_user_registration_path
+      fill_in "Email", with: "newemail@example.com"
+      fill_in "Current Password", with: user.password
+      click_button "Update"
+      page.should have_content "successfully"
+    end
+
+    scenario "logged in user is able to change their password" do
+      visit edit_user_registration_path
+      fill_in "Password", with: "NewPassword"
+      fill_in "Password Confirmation", with: "NewPassword"
+      fill_in "Current Password", with: user.password
+      click_button "Update"
+      page.should have_content "successfully"
+    end
   end
 end
