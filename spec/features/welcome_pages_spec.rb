b8186768d5b15265c212e6cf28ca3d403cd69245
Corey Norris <coreypnorris@gmail.com>
2014-07-21 10:11:03 -0700

    create integration test for #genre-selector

--- spec/features/welcome_pages_spec.rb
+++ spec/features/welcome_pages_spec.rb
@@ -28,12 +28,19 @@ feature "Viewing home page" do
   end
 
   scenario "User can filter issues by genre with genre buttons" do
-    issue_1 = FactoryGirl.create(:page).issue
-    issue_2 = FactoryGirl.create(:page).issue
-    visit root_path
-    click_link issue_2.title.genre.name
+    horror = FactoryGirl.create(:genre, :name => "Horror")
+    tomb_of_dracula = horror.titles.create(:name => "Tomb of Dracula")
+    western = FactoryGirl.create(:genre, :name => "Western")
+    two_gun_kid = western.titles.create(:name => "Two Gun Kid")
+    tomb_of_dracula_1 = tomb_of_dracula.issues.create(:number => 1, :cover =>"tomb_of_dracula_1_cover")
+    two_gun_kid_1 = two_gun_kid.issues.create(:number => 1, :cover =>"two_gun_kid_1_cover")
+    tomb_of_dracula_1.pages.create(:number => 1, :image => "page_1_image_url")
+    two_gun_kid_1.pages.create(:number => 1, :image => "page_1_image_url")
+    visit issue_page_path(tomb_of_dracula_1, tomb_of_dracula_1.pages.first)
 
-    page.should_not have_content issue_1
+    select("Horror", :from => "genre-selector")
+    click_button "Go to Genre"
+    page.should have_content tomb_of_dracula_1.title.name
   end
 
   scenario "User can search for issues with search bar" do
