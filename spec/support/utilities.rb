09abbbbc6a7c722d44851b617bf76ac0f2c647dc
Corey Norris <coreypnorris@gmail.com>
2014-07-24 13:08:21 -0700

    create create_page_and_post_comment method

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,6 +12,16 @@ def sign_in(user)
 end
 
 def sign_out
-  visit root_url
-  click_link "Sign Out"
+  visit root_path
+  click_on "Sign Out"
+end
+
+def create_page_and_post_comment
+  create_user
+  sign_in(@user)
+  @issue = FactoryGirl.create(:page).issue
+  visit issue_page_path(@issue, @issue.pages.first)
+  @comment = FactoryGirl.build(:comment, :user_id => @user.id)
+  fill_in "issue-#{@issue.id}-comment-body", :with => @comment.body
+  click_button "Comment on this Issue"
 end
