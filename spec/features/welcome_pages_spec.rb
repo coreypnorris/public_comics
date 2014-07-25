7ff23833332b43c0deab39e38a04b61f99d3f1f7
Corey Norris <coreypnorris@gmail.com>
2014-07-25 09:58:37 -0700

    move search bar and genre selector tests from welcome_pages_spec to navbar_pages_spec

--- spec/features/welcome_pages_spec.rb
+++ spec/features/welcome_pages_spec.rb
@@ -28,33 +28,4 @@ feature "Viewing home page" do
     page.should have_content old_issue.title.name
     page.should_not have_content new_issue.title.name
   end
-
-  scenario "User can filter issues by genre with genre buttons" do
-    horror = FactoryGirl.create(:genre, :name => "Horror")
-    tomb_of_dracula = horror.titles.create(:name => "Tomb of Dracula")
-    western = FactoryGirl.create(:genre, :name => "Western")
-    two_gun_kid = western.titles.create(:name => "Two Gun Kid")
-    tomb_of_dracula_1 = tomb_of_dracula.issues.create(:number => 1, :cover =>"tomb_of_dracula_1_cover")
-    two_gun_kid_1 = two_gun_kid.issues.create(:number => 1, :cover =>"two_gun_kid_1_cover")
-    tomb_of_dracula_1.pages.create(:number => 1, :image => "page_1_image_url")
-    two_gun_kid_1.pages.create(:number => 1, :image => "page_1_image_url")
-    visit issue_page_path(tomb_of_dracula_1, tomb_of_dracula_1.pages.first)
-
-    select("Horror", :from => "genre-selector")
-    click_button "Go to Genre"
-
-    page.should have_content tomb_of_dracula_1.title.name
-  end
-
-  scenario "User can search for issues with search bar", :retry => 3 do
-    issue_1 = FactoryGirl.create(:page).issue
-    issue_2 = FactoryGirl.create(:page).issue
-    visit root_path
-    fill_in 'search', with: issue_2.title.name
-    click_button 'Search'
-
-    page.should_not have_content issue_1.title.name
-    page.should have_content issue_2.title.name
-  end
-
 end
