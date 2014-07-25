0a4bf94eecef81cde4ea0812ad293f61410111fb
Corey Norris <coreypnorris@gmail.com>
2014-07-25 15:53:51 -0700

    create destroy method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -39,6 +39,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Your comment has been removed."
+    redirect_to :back
+  end
+
 private
   def comment_params
     params.require(:comment).permit(:body)
