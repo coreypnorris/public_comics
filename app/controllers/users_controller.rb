b8dd972508d6b7224de35464374a574f5353948b
Corey Norris <coreypnorris@gmail.com>
2014-07-28 11:36:53 -0700

    create update method for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
     @user = User.find_by_username(params[:id].downcase)
   end
 
+  def update
+    @user = User.find_by_username(params[:id])
+    if @user.update user_params
+      flash[:notice] = "Your avatar has been added."
+      redirect_to :back
+    else
+      flash[:error] = "Something went wrong. Please try to save your user again."
+      redirect_to :back
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:avatar)
+    end
+
 end
