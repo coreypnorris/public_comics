e2c863803d06bc3c8dffaeca5830fa44a6096e84
Corey Norris <coreypnorris@gmail.com>
2014-07-28 10:57:47 -0700

    add has_attached_file method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,16 @@ class User < ActiveRecord::Base
 
   acts_as_voter
 
+  # This method associates the attribute ":avatar" with a file attachment
+    has_attached_file :avatar, styles: {
+      thumb: '100x100>',
+      square: '200x200#',
+      medium: '300x300>'
+    }
+
+    # Validate the attached image is image/jpg, image/png, etc
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   def to_param
     username
   end
