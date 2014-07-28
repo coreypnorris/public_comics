a0f442f8b5f0daceabfd13d94fb5f75b0c6c4f86
Corey Norris <coreypnorris@gmail.com>
2014-07-28 10:41:39 -0700

    configure app for paperclip and aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,9 @@ PublicComics::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
 end
