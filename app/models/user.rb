08078ea1d21da937b83fcb850876b24954929e46
Corey Norris <coreypnorris@gmail.com>
2014-07-28 18:01:41 -0700

    add default url for avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,9 @@ class User < ActiveRecord::Base
   acts_as_voter
 
   # This method associates the attribute ":avatar" with a file attachment
-    has_attached_file :avatar, styles: {
-      comment: '85x85>',
-      square: '200x200#',
-      medium: '300x300>'
-    }
+    has_attached_file :avatar,
+      styles: { comment: '85x85>', square: '200x200#' },
+      :default_url => "/assets/:style/missing.png"
 
     # Validate the attached image is image/jpg, image/png, etc
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
