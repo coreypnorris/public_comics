ea063f2ef0f262e01dae2a72f5e35d9225e57c19
Corey Norris <coreypnorris@gmail.com>
2014-07-28 18:22:26 -0700

    refactor production paperclip settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ PublicComics::Application.configure do
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_host_name => "s3-us-west-2.amazonaws.com"
   }
 end
