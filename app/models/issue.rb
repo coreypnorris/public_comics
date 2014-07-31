5274a4386c22f7ac99fdfc57696b9add35c53d49
Corey Norris <coreypnorris@gmail.com>
2014-07-31 11:50:17 -0700

    getter and setter methods for title_name

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -7,4 +7,12 @@ class Issue < ActiveRecord::Base
   has_many :pages
   has_many :comments, :as => :commentable
   belongs_to :title
+
+  def title_name
+    title.try(:name)
+  end
+
+  def title_name=(name)
+    self.title = Title.find_by_name(name) if name.present?
+  end
 end
