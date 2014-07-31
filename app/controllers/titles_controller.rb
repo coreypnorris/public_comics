8cc8a57d61c46033bb2fade24f91185b96f4bdb7
Corey Norris <coreypnorris@gmail.com>
2014-07-31 12:55:50 -0700

    refactor index action for autocomplete

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -3,16 +3,22 @@ class TitlesController < ApplicationController
   require 'will_paginate/array'
 
   def index
-    @titles = Title.search(params[:search].titleize)
-    @issues = []
 
-    @titles.each do |title|
-      title.issues.each do |issue|
-        @issues << issue
+    if params[:term]
+      @titles = Title.order(:name).where("name like ?", "%#{params[:term].titleize}%")
+      render json: @titles.map(&:name)
+    else
+      @titles = Title.search(params[:search].titleize)
+      @issues = []
+
+      @titles.each do |title|
+        title.issues.each do |issue|
+          @issues << issue
+        end
       end
+      @issues = @issues.sort_by { |issue| issue.created_at }
+      @issues = @issues.reverse.paginate(:per_page => 12, :page => params[:page])
     end
-    @issues = @issues.sort_by { |issue| issue.created_at }
-    @issues = @issues.reverse.paginate(:per_page => 12, :page => params[:page])
   end
 
 private
