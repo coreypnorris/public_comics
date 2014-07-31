e21072c6d0d689220ac47733d89585aa43a4786e
Corey Norris <coreypnorris@gmail.com>
2014-07-31 12:59:29 -0700

    add title instance variable for form

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,9 +1,10 @@
 class IssuesController < ApplicationController
   before_filter :authenticate_user!
 
-def new
-  @issue = Issue.new
-end
+  def new
+    @issue = Issue.new
+    @title = Title.new
+  end
 
 private
   def issue_params
