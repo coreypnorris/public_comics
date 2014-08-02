098d3f25b78d868738bb91ff1c8cc85370470f31
Corey Norris <coreypnorris@gmail.com>
2014-08-02 12:15:12 -0700

    refactor selector in autocomplete script

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -1,6 +1,6 @@
 // Autocomplete for title name in new issue form
 jQuery(function() {
-  return $('#issue_title_name').autocomplete({
-    source: $('#issue_title_name').data('autocomplete-source')
+  return $('#title_name').autocomplete({
+    source: $('#title_name').data('autocomplete-source')
   });
 });
