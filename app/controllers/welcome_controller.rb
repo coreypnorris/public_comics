fc476c2b97b02e63df20262a920de835728e694f
Corey Norris <coreypnorris@gmail.com>
2014-08-02 14:41:17 -0700

    refactor index method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,15 +3,7 @@ class WelcomeController < ApplicationController
   require 'will_paginate/array'
 
   def index
-    @titles = Title.all
-    @issues = []
-
-    Title.all.each do |title|
-      title.issues.each do |issue|
-        @issues << issue
-      end
-    end
-    @issues = @issues.sort_by { |issue| issue.created_at }
+    @issues = Issue.all.sort_by { |issue| issue.created_at }
     @issues = @issues.reverse.paginate(:per_page => 12, :page => params[:page])
   end
 
