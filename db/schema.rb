e0e981056aa5b39d7f0b2d41ed9abaf3dbe3abaf
Corey Norris <coreypnorris@gmail.com>
2014-08-02 14:20:23 -0700

    add user has_many issues association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140802204142) do
+ActiveRecord::Schema.define(version: 20140802211526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20140802204142) do
     t.string   "cover_content_type"
     t.integer  "cover_file_size"
     t.datetime "cover_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "pages", force: true do |t|
