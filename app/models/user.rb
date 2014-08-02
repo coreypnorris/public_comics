6d466cfa9748b56ec68527b10c5a1c962c85e7ed
Corey Norris <coreypnorris@gmail.com>
2014-08-02 12:25:57 -0700

    fix indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ActiveRecord::Base
   acts_as_voter
 
   # This method associates the attribute ":avatar" with a file attachment
-    has_attached_file :avatar,
-      styles: { comment: '85x85>', profile: '150x150#' },
-      :default_url => "/assets/:style/missing.png"
+  has_attached_file :avatar,
+    styles: { comment: '85x85>', profile: '150x150#' },
+    :default_url => "/assets/:style/missing.png"
 
-    # Validate the attached image is image/jpg, image/png, etc
-    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def to_param
     username
