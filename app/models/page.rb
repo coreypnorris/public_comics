8219a14b9d06ee80d27000647c316321d0ffcf22
Corey Norris <coreypnorris@gmail.com>
2014-08-02 13:44:32 -0700

    add paperclip configurations for images

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,12 @@
 class Page < ActiveRecord::Base
-  validates :image, :presence => true
   validates :number, :presence => true
 
   belongs_to :issue
+
+  has_attached_file :image
+
+  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 
   def to_param
     number.to_s
