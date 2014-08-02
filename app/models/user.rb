3ecc6bf9587d9284c95021557e2488c4b287623d
Corey Norris <coreypnorris@gmail.com>
2014-08-02 14:50:24 -0700

    check for unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :username, :presence => true
+  validates :username, :uniqueness => true
   has_many :comments
   has_many :issues
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,6 @@ require 'spec_helper'
 
 describe User do
   it { should validate_presence_of :username }
+  it { should validate_uniqueness_of :username }
   it { should have_many :issues }
 end
