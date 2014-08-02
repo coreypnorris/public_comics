872efd80d9126091d4d6e22fd00ef422188ad686
Corey Norris <coreypnorris@gmail.com>
2014-08-02 14:40:41 -0700

    add methods to pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,27 @@
 class PagesController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
 
   def index
     @issue = Issue.find(params[:issue_id])
     @pages = @issue.pages
+  end
+
+  def new
+    @issue = Issue.find(params[:issue_id])
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    @issue = Issue.find(params[:issue_id])
+    if @issue.save
+      @issue.pages << @page
+      flash[:notice] = "Your page has been added to #{@issue.title.name} ##{@issue.number}."
+      redirect_to new_issue_page_path(@issue)
+    else
+      flash[:alert] = "Something went wrong. Please try to save your page again."
+      redirect_to :back
+    end
   end
 
   def show
@@ -12,8 +31,15 @@ class PagesController < ApplicationController
     @comment = Comment.new
   end
 
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    flash[:notice] = "Your page has been removed."
+    redirect_to :back
+  end
+
 private
   def page_params
-    params.require(:page).permit(:image, :issue_id)
+    params.require(:page).permit(:image, :number, :issue_id)
   end
 end
