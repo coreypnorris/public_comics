f92c3561703c1c0fd9db790f5929e577d18ae36d
Corey Norris <coreypnorris@gmail.com>
2014-08-02 12:17:57 -0700

    create paperclip configurations for issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,11 +2,16 @@ class Issue < ActiveRecord::Base
   acts_as_commentable
 
   validates :number, :presence => true
-  validates :cover, :presence => true
 
   has_many :pages
   has_many :comments, :as => :commentable
   belongs_to :title
+
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :cover
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
 
   def search
     title.try(:name)
