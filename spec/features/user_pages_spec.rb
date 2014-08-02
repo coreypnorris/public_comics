147be0afea27f69722a7c148e0cc4ce4a4765520
Corey Norris <coreypnorris@gmail.com>
2014-08-02 16:07:50 -0700

    refactor test for uploading avatars

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -5,7 +5,8 @@ feature 'Viewing profile' do
   before { visit user_path(@user) }
 
   scenario 'user can upload an avatar', :retry => 5 do
-    attach_file('user_avatar', File.join(Rails.root, '/spec/support/test_avatar.png'))
-    page.should have_content "Username: #{@user.username}"
+    page.attach_file('user_avatar', File.join(Rails.root, '/spec/support/test_avatar.png'))
+    click_button 'Upload Image'
+    page.should have_content("Your avatar has been added.")
   end
 end
