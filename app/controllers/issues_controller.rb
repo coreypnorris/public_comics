25c32ca039b5815572c5e1e092047d22d5f42390
Corey Norris <coreypnorris@gmail.com>
2014-08-02 12:16:22 -0700

    create issue create method

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,7 +6,16 @@ class IssuesController < ApplicationController
   end
 
   def create
-
+    @title = Title.find_or_initialize_by_name(params[:title_name].titleize)
+    @issue = Issue.new(issue_params)
+    if @title.save && @issue.save
+      @title.issues << @issue
+      flash[:notice] = "Your title has been submitted."
+      redirect_to new_issue_page_path(@issue)
+    else
+      flash[:alert] = "Something went wrong. Please try to save your issue again."
+      redirect_to :back
+    end
   end
 
 private
