544c43005110bd9c17e4106e024ac54839d59b22
Corey Norris <coreypnorris@gmail.com>
2014-08-03 15:23:09 -0700

    make associations before saving objects

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,8 @@
 class IssuesController < ApplicationController
   before_filter :authenticate_user!
 
+  require 'will_paginate/array'
+
   def index
     @issues = Issue.all.sort_by { |issue| issue.created_at }
     @issues = @issues.reverse.paginate(:per_page => 12, :page => params[:page])
@@ -14,10 +16,10 @@ class IssuesController < ApplicationController
     @title = Title.find_or_initialize_by_name(params[:title_name].titleize)
     @genre = Genre.find_or_initialize_by_name(params[:title_genre_name].titleize)
     @issue = Issue.new(issue_params)
+    @genre.titles << @title
+    @title.issues << @issue
+    current_user.issues << @issue
     if @title.save && @genre.save && @issue.save
-      @genre.titles << @title
-      @title.issues << @issue
-      current_user.issues << @issue
       flash[:notice] = "Your issue has been submitted."
       redirect_to new_issue_page_path(@issue)
     else
@@ -28,6 +30,6 @@ class IssuesController < ApplicationController
 
 private
   def issue_params
-    params.require(:issue).permit(:number, :cover, :user_id, :title_id)
+    params.require(:issue).permit(:number, :cover, :approved, :user_id, :title_id)
   end
 end
