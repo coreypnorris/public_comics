c39f85c21d761cdcd9763d900ad6f3758fa6b6d1
Corey Norris <coreypnorris@gmail.com>
2014-08-03 15:27:27 -0700

    i only show approved issues in welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   require 'will_paginate/array'
 
   def index
-    @issues = Issue.all.sort_by { |issue| issue.created_at }
+    @issues = Issue.approved.sort_by { |issue| issue.created_at }
     @issues = @issues.reverse.paginate(:per_page => 12, :page => params[:page])
   end
 
