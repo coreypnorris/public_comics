43a64a3e4b9a1f89e2c6cd6d53470d8e36e98807
Corey Norris <coreypnorris@gmail.com>
2014-08-02 22:33:17 -0700

    refactor factories

---
+++ spec/factories/factories.rb
@@ -0,0 +1,37 @@
+include ActionDispatch::TestProcess
+
+FactoryGirl.define do
+  factory :user do
+    username { Faker::Internet.user_name }
+    email { Faker::Internet.email }
+    password 'foobarbaz'
+    password_confirmation 'foobarbaz'
+  end
+
+  factory :comment do
+    user
+    body { Faker::Lorem.sentence }
+  end
+
+  factory :genre do
+    name { Faker::Name.name }
+  end
+
+  factory :title do
+    genre
+    name { Faker::Name.name }
+  end
+
+  factory :issue do |i|
+    user
+    title
+    i.number { Faker::Number.number(3) }
+    i.cover { fixture_file_upload( 'spec/factories/test_cover.jpg', 'image/jpg') }
+  end
+
+  factory :page do |p|
+    issue
+    p.image { fixture_file_upload( 'spec/factories/test_page_1.jpg', 'image/jpg') }
+    p.number 1
+  end
+end
