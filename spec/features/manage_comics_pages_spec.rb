aa9e7f764cffe89a139658a625f2207a97b6cb5a
Corey Norris <coreypnorris@gmail.com>
2014-08-15 20:11:18 -0700

    add test for seeing a users uploaded comics

--- spec/features/manage_comics_pages_spec.rb
+++ spec/features/manage_comics_pages_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 feature 'A user managing his/her uploaded comics' do
   before { create_and_sign_in_user }
-  before { visit user_issues_path(@user.username) }
 
-  scenario "a user hasnt uploaded any comics", :retry => 5 do
+  scenario "a user hasn't uploaded any comics", :retry => 5 do
+    visit user_issues_path(@user.username)
     page.should have_content("You havent uploaded any comics yet.")
+  end
+
+  scenario "a user can see the comics they've uploaded" do
+    @issue = FactoryGirl.create(:issue, :user_id => @user.id)
+    visit user_issues_path(@user.username)
+    page.should have_content @issue.title.name
   end
 end
