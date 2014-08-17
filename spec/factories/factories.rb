5d86e42ded73800bd3f4661eca87d46d63673478
Corey Norris <coreypnorris@gmail.com>
2014-08-16 21:13:56 -0700

    write test deleting comics

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :user do
     username { Faker::Internet.user_name }
     email { Faker::Internet.email }
+    admin true
     password 'foobarbaz'
     password_confirmation 'foobarbaz'
   end
--- spec/features/manage_comics_pages_spec.rb
+++ spec/features/manage_comics_pages_spec.rb
@@ -13,4 +13,11 @@ feature 'A user managing his/her uploaded comics' do
     visit user_issues_path(@user.username)
     page.should have_content @issue.title.name
   end
+
+  scenario "a can delete a comic they've uploaded", :retry => 5 do
+    @issue = FactoryGirl.create(:issue, :user_id => @user.id)
+    FactoryGirl.create(:page, :issue_id => @issue.id)
+    visit user_issues_path(@user.username)
+    click_link "issue-#{@issue.id}-delete-btn"
+  end
 end
