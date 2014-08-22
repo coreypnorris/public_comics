be14c7a357abc35c75c2add3c5e7848762a0fdef
Corey Norris <coreypnorris@gmail.com>
2014-08-22 16:32:19 -0700

    refactor test to upload multiple images

--- spec/features/upload_comics_pages_spec.rb
+++ spec/features/upload_comics_pages_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
-feature 'A user uploading a comic' do
+feature 'A user uploading comics' do
   before { create_and_sign_in_user }
   let!(:test_user) { FactoryGirl.build(:user) }
   let!(:test_issue) { FactoryGirl.build(:issue, :user_id => test_user.id) }
   let!(:test_page) { FactoryGirl.build(:page, :issue_id => test_issue.id) }
 
-  scenario "a user hasn't uploaded any comics", :retry => 5 do
+  scenario "a user uploading a comic", :retry => 5 do
     visit new_issue_path
     page.should have_content("New Issue")
     page.attach_file('new-issue-pages', File.join(Rails.root, '/spec/support/test_image.png'))
+    page.attach_file('new-issue-pages', File.join(Rails.root, '/spec/support/test_image_2.jpg'))
+    page.attach_file('new-issue-pages', File.join(Rails.root, '/spec/support/test_image_3.jpg'))
     fill_in 'title_name', :with => test_issue.title.name
     fill_in 'Issue Number', :with => test_issue.number
     fill_in 'title_genre_name', :with => test_issue.title.genre
