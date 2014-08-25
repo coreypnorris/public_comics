85553a88434624902eaa39644d70908b0705575e
Corey Norris <coreypnorris@gmail.com>
2014-08-24 21:29:43 -0700

    Update seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-User.create(:username => "admin", :email => "coreypnorris@gmail.com", :password => ENV['ADMIN_PASSWORD'], :password_confirmation => ENV['ADMIN_PASSWORD'], :admin => true)
+User.create(:username => "admin", :email => "admin@example.com", :password => ENV['ADMIN_PASSWORD'], :password_confirmation => ENV['ADMIN_PASSWORD'], :admin => true)
 User.create(:username => "testuser", :email => "testuser@example.com", :password => ENV['TEST_USER_PASSWORD'], :password_confirmation => ENV['TEST_USER_PASSWORD'], :admin => false)
