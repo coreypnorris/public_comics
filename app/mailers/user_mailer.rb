8fb6eb549e1d3fa3f2f4cee77272c454037f07a1
Corey Norris <coreypnorris@gmail.com>
2014-08-31 21:24:04 -0700

    refactor devise emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,23 @@
 class UserMailer < ActionMailer::Base
-  default from: "admin@publiccomics.com"
+  include Devise::Mailers::Helpers
+
+  default from: "admin@publiccomics.herokuapp.com"
 
   def signup_confirmation(user)
     @user = user
 
     mail to: user.email, subject: "Sign Up Confirmation"
+  end
+
+  def confirmation_instructions(record)
+    devise_mail(record, :confirmation_instructions)
+  end
+
+  def reset_password_instructions(record)
+    devise_mail(record, :reset_password_instructions)
+  end
+
+  def unlock_instructions(record)
+    devise_mail(record, :unlock_instructions)
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -8,11 +8,26 @@ describe UserMailer do
     it "renders the headers" do
       mail.subject.should eq("Sign Up Confirmation")
       mail.to.should eq(["#{user.email}"])
-      mail.from.should eq(["admin@publiccomics.com"])
+      mail.from.should eq(["admin@publiccomics.herokuapp.com"])
     end
 
     it "renders the body" do
       mail.body.encoded.should match("Thank you for signing up.")
+    end
+  end
+
+  describe "forgotton password" do
+    let(:user) { FactoryGirl.create(:user)}
+    let(:mail) { UserMailer.reset_password_instructions(user) }
+
+    it "renders the headers" do
+      mail.subject.should eq("Reset password instructions")
+      mail.to.should eq(["#{user.email}"])
+      mail.from.should eq(["admin@publiccomics.herokuapp.com"])
+    end
+
+    it "renders the body" do
+      mail.body.encoded.should match("Someone has requested a link to change your password. You can do this through the link below.")
     end
   end
 
