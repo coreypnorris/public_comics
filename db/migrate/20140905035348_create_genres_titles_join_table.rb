77efc333a0185ec57d51b5330c2996bc63927e17
Corey Norris <coreypnorris@gmail.com>
2014-09-06 12:56:01 -0700

    remove broken migration

--- db/migrate/20140905035348_create_genres_titles_join_table.rb
+++
@@ -1,7 +0,0 @@
-class CreateGenresTitlesJoinTable < ActiveRecord::Migration
-  def change
-    create_table :genres_titles, id: false do |t|
-    t.integer :genre_id, null: false
-    t.integer :title_id, null: false
-  end
-end
