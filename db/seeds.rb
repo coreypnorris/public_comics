666f64108180a3e4ebd8f69ab2d0ee4437ca03d9
Corey Norris <coreypnorris@gmail.com>
2014-09-06 12:58:10 -0700

    refactor passwords in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-User.create(:username => "admin", :email => "admin@example.com", :password => "111111", :password_confirmation => "111111", :admin => true)
-User.create(:username => "testuser", :email => "testuser@example.com", :password => "111111", :password_confirmation => "111111", :admin => false)
+User.create(:username => "admin", :email => "admin@example.com", :password => "11111111", :password_confirmation => "11111111", :admin => true)
+User.create(:username => "testuser", :email => "testuser@example.com", :password => "11111111", :password_confirmation => "11111111", :admin => false)
